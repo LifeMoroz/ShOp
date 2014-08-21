e37f1554e2ce7f27234b11575cd1fd6cf1a7c91a
Galimov Ruslan <delfin1995@yandex.ru>
2014-08-22 02:53:03 +0400

    Start work with goods.html

--- js/index.js
+++ js/index.js
@@ -25,7 +25,7 @@ function control_size() {
         $(".header_link_auth").css({'background-size': "22% "+$(".header_link_auth").width()*0.3+"px"});
         $(".header_link_cart").css({'background-size': "22% "+$(".header_link_cart").width()*0.3+"px"});
         $(".header_no_link_phone").css({'font-size': $(".header_no_link_phone").width()*0.13});
-        $(".header_link_auth").css({'font-size': $(".header_link_auth").width()*0.13});
+        $(".header_link_auth").css({'font-size': $(".header_link_auth").width()*0.115});
         $(".header_link_cart").css({'font-size': $(".header_link_cart").width()*0.13});
         /*** <--END HEADER--> ***/
 
